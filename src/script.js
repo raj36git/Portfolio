b1ad27b3f59fddb146426b5e8c6e754d8480c8ed
raj36git <vector36razz@gmail.com>
2024-12-20 18:28:58 +0530

    Navbar in portfolio updated

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const navDialog = document.getElementById("nav-dialog");
+
+function toggleMenu() {
+  navDialog.classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Baloo Bhaijaan 2", "sans-serif"],
+        body: ["Poppins", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
